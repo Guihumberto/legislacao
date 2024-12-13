fbe50ea8d57882367be343d118854eff7d0a1be7
01791743390 <joao.junior@sefaz.ma.gov.br>
2024-12-13 09:33:35 -0300

    postcreate title and subtitle

--- src/components/Posts/create/createPost.vue
+++ src/components/Posts/create/createPost.vue
@@ -20,7 +20,56 @@
                     clearable
                     :rules="[rules.minfield, rules.required]"
                ></v-text-field>
-               <v-textarea
+               <v-btn 
+                @click="showref = !showref"
+                class="text-lowercase" variant="text" color="success" prepend-icon="mdi-plus">Incluir referências</v-btn>
+               <v-expand-transition>
+                   <div v-if="showref">
+                    <v-btn prepend-icon="mdi-link" variant="outlined" class="text-lowercase" >incluir a partir dos meus documentos</v-btn>
+                    <v-form @submit.prevent="addRef()" ref="formref">
+                        <v-text-field
+                            label="Referência"
+                            density="compact"
+                            variant="outlined"
+                            class="mt-5"
+                            placeholder="Exemplo: art. 77, do CTE-MA (lei 7.799/2010)"
+                            clearable
+                            v-model="textref"
+                            :rules="[rules.required, rules.minfield]"
+                        >
+                            <template v-slot:append>
+                                <v-btn type="submit" density="compact" color="success" icon="mdi-plus"></v-btn>
+                            </template>
+                        </v-text-field>
+                    </v-form>
+                    <div class="listref" v-if="listRef.length">
+                        <div class="d-flex align-center">
+                            <v-icon class="mr-2">mdi-format-list-bulleted</v-icon>
+                            <h3>Lista de Referências</h3>
+                        </div>
+                        <v-list class="pa-0 mt-5">
+                            <v-list-item v-for="item, i in listRef" :key="i" class="border-b" link>
+                            <template v-slot:prepend>
+                                <v-icon>
+                                    mdi-note-text-outline
+                                </v-icon>
+                            </template>
+                            <template v-slot:append>
+                                <v-switch label="Publicar" class="mr-2" hide-details color="success"></v-switch>
+                                <OptionsRef />
+                            </template>
+                            <div>
+                                <span class="font-weight-bold">Title</span> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quod doloremque consecte
+                            </div>
+                            </v-list-item>
+                        </v-list>
+                    </div>
+                   </div>
+               </v-expand-transition>
+               <div class="text-right">
+                   <v-btn :disabled="!post.title" type="submit" variant="flat" color="primary">Salvar e continuar</v-btn>
+               </div>
+               <!-- <v-textarea
                     label="Texto"
                     placeholder="Escreva o texto"
                     variant="outlined"
@@ -47,10 +96,7 @@
                     clearable
                     :rules="[rules.minfield, rules.required]"
                ></v-text-field>
-               <v-switch label="Publicar" v-model="post.publish" color="success">Publicar</v-switch>
-               <div class="text-right">
-                   <v-btn type="submit" variant="flat" color="primary">Salvar</v-btn>
-               </div>
+               <v-switch label="Publicar" v-model="post.publish" color="success">Publicar</v-switch> -->   
             </v-form>
         </v-card-text>
     </v-card>
@@ -58,26 +104,45 @@
 
 <script setup>
 import { ref } from 'vue';
+import OptionsRef from './optionsRef.vue';
+
+const showref = ref(false)
+const textref = ref(null)
+
 
 const post = ref({
     title: null, 
     subtitle: null, 
+    imgLink: null,
     text: null,
     tags: [],
     references: [],
-    publish: false
+    publish: false,
 })
 
 const form = ref(null)
+const formref = ref(false)
 
 const rules = {
     required: value => !!value || "campo obrigatório", 
     minfield: (v) => (v||'').length >= 3 || "Mínimo 4 caracteres",
 }
 
+const listRef = ref([1])
+
 const savePost = async () => {
-        const { valid } = await form.value.validate()
-        if(valid) {}
+    const { valid } = await form.value.validate()
+        if(valid) {
+            console.log('salvar', post.value);
+        }
+}
+
+const addRef = async () => {
+        const { valid } = await formref.value.validate()
+        if(valid) {
+            listRef.value.push(textref.value);
+            textref.value = null
+        }
 }
 </script>
 
---
+++ src/components/Posts/create/optionsRef.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="text-center">
+      <v-menu location="top">
+        <template v-slot:activator="{ props }">
+          <v-btn density="compact" variant="text" icon="mdi-dots-vertical" v-bind="props"></v-btn>
+        </template>
+        <v-list class="pa-0">
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            link
+          >
+            <template v-slot:prepend>
+                <v-icon>
+                    {{ item.icon }}
+                </v-icon>
+            </template>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </template>
+  <script>
+    export default {
+      data: () => ({
+        items: [
+          { title: 'Editar', icon: 'mdi-pencil'},
+          { title: 'Excluir', icon: 'mdi-delete' },
+        ],
+        locations: [
+          'top',
+          'bottom',
+          'start',
+          'end',
+          'center',
+        ],
+        location: 'end',
+      }),
+    }
+  </script>
